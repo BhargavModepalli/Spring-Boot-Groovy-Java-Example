69b04c62d0de80b1dbac60a5b6473634ed589db6
Bhargav Modepalli <Modepalli.Bhargav@aoins.com>
2019-05-31 11:02:18 -0400

    Initial commit

---
+++ src/main/java/com/example/ApplicationGroovy.groovy
@@ -0,0 +1,18 @@
+package com.example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration.class, JmsAutoConfiguration.class])
+@ComponentScan(basePackages = "com.example")
+class ApplicationGroovy {
+ static void main(String[] args) {
+        SpringApplication.run(ApplicationGroovy.class, args)
+    }
+
+}
